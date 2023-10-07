1f27ecca5767435b2138d9dfe8cd25f61c82f4fd
GustavoMartins <gtavomartins893@gmail.com>
2023-10-07 17:05:14 -0300

    adicionando tags nas publicacoes

--- src/components/Publication/Publication.jsx
+++ src/components/Publication/Publication.jsx
@@ -3,8 +3,9 @@ import './Publication.css';
 import user_img from '../../assets/user.png';
 import earth_img from '../../assets/earth.jpg';
 
-// user, project, title, description, userImg, publicationImg
 const Publication = ({ data }) => {
+
+  const {userName, project, title, description, categories, userImg, publicationImg} = data;
 
   // Para Teste (remover dps)
   // userImg = user_img;
@@ -17,21 +18,27 @@ const Publication = ({ data }) => {
 
       <div className="user-info">
 
-        <img className="user-img" src={data.userImg} alt="Imagem do Usuário" />
+        <img className="user-img" src={userImg} alt="Imagem do Usuário" />
         <p className="user-name">
-          User Name /{" "} {/* {data.user} */}
+          User Name /{" "} {/* {userName} */}
         </p>
         <span className="project-link" onClick={() => navigate('view-projects/2')}>
-          Project Name{/* {data.projectName} */}
+          Project Name{/* {projectName} */}
         </span>
 
       </div>
       
       <div className="publication-content">
-        <h2 className="publication-title">Publication Title</h2>  {/* {data.title} */}
-        {/* data.categoria */}
-        <p className="publication-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>  {/* {data.description} */}
-        <img className="publication-img" src={data.publicationImg} alt="Imagem da Publicação" />
+        <h2 className="publication-title">Publication Title</h2>  {/* {title} */}
+        
+        {categories.map((category) => (
+          <div key={category.name} className="category">
+            {category.name}
+          </div>
+        ))}
+
+        <p className="publication-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>  {/* {description} */}
+        <img className="publication-img" src={publicationImg} alt="Imagem da Publicação" />
       </div>
 
     </div>
