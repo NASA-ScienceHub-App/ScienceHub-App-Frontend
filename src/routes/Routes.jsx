77683183c9da47b7ea67b862ad1babab442b0dcf
diogorodriguees <diogorodrigueslife@gmail.com>
2023-10-07 14:19:45 -0300

    :sparkles: pagina de login adicionada

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home.jsx";
 import Projects from "../pages/Projects/Projects.jsx";
+import Login from "../pages/Login/Login.jsx";
 
 const Routering = createBrowserRouter([
     {
         path: "/",
+        element: <Login />,
+    },
+    {
+        path: "/home",
         element: <Home />,
     },
     {
